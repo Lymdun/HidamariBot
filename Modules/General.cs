ea020b20ffe8df0807375c02e26f99948dea9efa
Lymdun <lymdun@protonmail.com>
2024-04-08 23:59:25 +0200

    remove everyone command

--- Modules/General.cs
+++ Modules/General.cs
@@ -1,10 +1,7 @@
-﻿using Disqord;
-using Disqord.Bot.Commands;
+﻿using Disqord.Bot.Commands;
 using Disqord.Bot.Commands.Application;
-using Disqord.Rest;
 using HidamariBot.Services;
 using Qmmands;
-using Serilog;
 
 namespace HidamariBot.Modules;
 
@@ -20,17 +17,4 @@ public class General : HidamariBotModuleBase {
         await QuizService.AddQuestionAsync(characterName, questionText);
         return Response("Question enregistrée avec succès.");
     }
-
-    /*[SlashCommand("everyone"), Description("[Oni] Pour everyone en passant par les pseudos."), RequireAuthorRole(481655718936707083)]
-    public async Task<IResult> Everyone() {
-        Snowflake guildId = this.Context.GuildId.GetValueOrDefault();
-        IReadOnlyList<IMember> members = await this.Bot.FetchMembersAsync(guildId);
-
-        IEnumerable<string> mentions = members.Select(x => x.Mention);
-        string? message = string.Join(", ", mentions);
-
-        Log.Information(message ?? "");
-
-        return Response("Test (Kud sale fils de pute)");
-    }*/
 }
