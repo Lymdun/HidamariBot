ba85a24f451e86c2ebef2ccd5bcec3889532789d
Lymdun <lymdun@protonmail.com>
2024-08-02 23:23:45 +0200

    refactor: simplify scheduler service

--- Models/Meme.cs
+++ Models/Meme.cs
@@ -1,19 +1,17 @@
 ﻿namespace HidamariBot.Models;
 
 public struct Meme {
-    public DayOfWeek DayOfWeek;
     public string ImageUrl;
 
-    Meme(DayOfWeek dayOfWeek, string imageUrl) {
-        this.DayOfWeek = dayOfWeek;
+    Meme(string imageUrl) {
         this.ImageUrl = imageUrl;
     }
 
     public static Meme MondayMeme() {
-        return new Meme(DayOfWeek.Monday, "makise_monday.webm");
+        return new Meme("makise_monday.webm");
     }
 
     public static Meme TuesdayMeme() {
-        return new Meme(DayOfWeek.Tuesday, "everybody.webm");
+        return new Meme("everybody.webm");
     }
 }
--- Services/SchedulerService.cs
+++ Services/SchedulerService.cs
@@ -1,4 +1,5 @@
-﻿using Disqord;
+﻿using Cronos;
+using Disqord;
 using Disqord.Bot.Hosting;
 using Disqord.Gateway;
 using Disqord.Rest;
@@ -10,83 +11,67 @@ namespace HidamariBot.Services;
 public class SchedulerService : DiscordBotService {
     const ulong CHANNEL_ID = 1203397714935549962;
     static readonly TimeZoneInfo frenchTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
-    int lastQuestionDayExecuted;
-    int lastMemeDayExecuted;
 
-    protected override ValueTask OnReady(ReadyEventArgs e)
-    {
+    const string CRON_EXPRESSION = "30 0 12 * * 1,2";
+
+    protected override ValueTask OnReady(ReadyEventArgs e) {
         Logger.LogInformation("SchedulerService Ready fired!");
         return default;
     }
 
-    // https://pgroene.wordpress.com/2018/05/31/run-scheduled-background-tasks-in-asp-net-core/
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
-        var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
-        while (await timer.WaitForNextTickAsync(stoppingToken)) {
-            DateTime today = DateTime.UtcNow;
-            TimeSpan waitEveningTime = GetEveningTime();
-            TimeSpan waitMorningTime = GetMorningTime();
+        var cronExpression = CronExpression.Parse(CRON_EXPRESSION);
 
-            if (Math.Abs(waitEveningTime.TotalSeconds) <= 5 && lastQuestionDayExecuted != today.DayOfYear) {
-                await SendMessage(stoppingToken);
-                lastQuestionDayExecuted = DateTime.UtcNow.DayOfYear;
-                Logger.LogInformation("SendMessage fired!");
-            } else if (Math.Abs(waitMorningTime.TotalSeconds) <= 5 && lastMemeDayExecuted != today.DayOfYear) {
-                switch (today.DayOfWeek) {
-                    case DayOfWeek.Monday:
-                        await SendMeme(stoppingToken, Meme.MondayMeme());
-                        lastMemeDayExecuted = DateTime.UtcNow.DayOfYear;
-                        Logger.LogInformation("SendMorningMessage fired!");
-                        break;
-                    case DayOfWeek.Tuesday:
-                        await SendMeme(stoppingToken, Meme.TuesdayMeme());
-                        lastMemeDayExecuted = DateTime.UtcNow.DayOfYear;
-                        Logger.LogInformation("SendMorningMessage fired!");
-                        break;
+        while (!stoppingToken.IsCancellationRequested) {
+            DateTimeOffset? nextOccurrence = cronExpression.GetNextOccurrence(DateTimeOffset.UtcNow, frenchTimeZone);
+
+            if (nextOccurrence.HasValue) {
+                TimeSpan delay = nextOccurrence.Value - DateTimeOffset.UtcNow;
+
+                if (delay > TimeSpan.Zero) {
+                    await Task.Delay(delay, stoppingToken);
                 }
+
+                try {
+                    await SendDailyMemeIfApplicable(nextOccurrence.Value.DayOfWeek, stoppingToken);
+                } catch (Exception ex) {
+                    Logger.LogError(ex, "Failure while sending meme");
+                }
+            } else {
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
             }
         }
 
-        if (stoppingToken.IsCancellationRequested)
-            Logger.LogWarning("StoppingToken cancellation requested");
+        Logger.LogWarning("SchedulerService has stopped");
     }
 
-    async Task SendMessage(CancellationToken stoppingToken) {
-        QuestionItem? question = await QuizService.GetCurrentQuestionAsync();
-        if (question != null) {
-            LocalEmbed embed = new LocalEmbed()
-                .WithColor(Color.Orange)
-                .WithTitle(question.CharacterName)
-                .WithDescription(question.QuestionText);
-
-            string? characterImage = await QuizService.FetchRandomImage(question.CharacterName);
-            if (!string.IsNullOrEmpty(characterImage))
-                embed.WithImageUrl(characterImage);
-
-            await Bot.SendMessageAsync(CHANNEL_ID, new LocalMessage().WithEmbeds(embed), cancellationToken: stoppingToken);
+    async Task SendDailyMemeIfApplicable(DayOfWeek dayOfWeek, CancellationToken stoppingToken) {
+        switch (dayOfWeek) {
+            case DayOfWeek.Monday:
+                await SendMeme(stoppingToken, Meme.MondayMeme());
+                Logger.LogInformation("SendMorningMessage fired!");
+                break;
+            case DayOfWeek.Tuesday:
+                await SendMeme(stoppingToken, Meme.TuesdayMeme());
+                Logger.LogInformation("SendMorningMessage fired!");
+                break;
         }
     }
 
     async Task SendMeme(CancellationToken stoppingToken, Meme meme) {
         var message = new LocalMessage();
-        await using (FileStream fs = File.OpenRead($"./resources/{meme.ImageUrl}")) {
+        string filePath = Path.Combine("./resources", meme.ImageUrl);
+
+        if (!File.Exists(filePath)) {
+            Logger.LogError("This meme does not exist: {FilePath}", filePath);
+            return;
+        }
+
+        await using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
             message.WithAttachments(LocalAttachment.File(fs));
             await Bot.SendMessageAsync(CHANNEL_ID, message, cancellationToken: stoppingToken);
         }
     }
-
-    TimeSpan GetEveningTime() {
-        DateTime frenchTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, frenchTimeZone);
-        return new TimeSpan(22, 22, 30) - frenchTime.TimeOfDay;
-    }
-
-    TimeSpan GetMorningTime() {
-        DateTime frenchTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, frenchTimeZone);
-        return new TimeSpan(12, 00, 30) - frenchTime.TimeOfDay;
-    }
-
-    DateTime GetNextOccurrence() => TimeZoneInfo.ConvertTimeFromUtc(
-        new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day + 1, 22, 23, 00), frenchTimeZone);
 
     public static TimeZoneInfo GetTimeZoneInfo() => frenchTimeZone;
 }
