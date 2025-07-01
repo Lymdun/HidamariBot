654e1fb4980677eddf4087c14b03568d5ee4e977
LaNotice <36107542+LaNotice@users.noreply.github.com>
2025-07-01 18:45:52 +0200

    change guild name and extraction from joining message

--- Services/MemberJoinService.cs
+++ Services/MemberJoinService.cs
@@ -10,6 +10,7 @@ public class MemberJoinService : DiscordBotService {
     const ulong NOTIFICATION_CHANNEL_ID = 1203052960712622110;
     const string IMAGE_PATH = "./resources/chezlymdun.png";
 
+    const string GUILD_NAME = "50 Nuances de Kikoojaps";
     const string QUESTIONNAIRE_MESSAGE_PATH =
         "https://discord.com/channels/450280270654865439/1203052960712622110/1203439457248743425";
 
@@ -27,7 +28,7 @@ public class MemberJoinService : DiscordBotService {
         try {
             var message = new LocalMessage()
                 .WithContent(
-                    $"{Mention.User(newMember.Id)} Bienvenue sur le reversed, pour passer la frontière il faut remplir le questionnaire {QUESTIONNAIRE_MESSAGE_PATH} 📋");
+                    $"{Mention.User(newMember.Id)} Bienvenue sur {GUILD_NAME}, pour passer la frontière il faut remplir le questionnaire {QUESTIONNAIRE_MESSAGE_PATH} 📋");
 
             if (File.Exists(IMAGE_PATH)) {
                 await using (var fs = new FileStream(IMAGE_PATH, FileMode.Open, FileAccess.Read)) {
